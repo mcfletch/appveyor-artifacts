dcb72f569176edbb38c83cd23b8fb01e69a1eb57
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-11-12 11:01:02 -0500

    Create missing directories as e.g. dist/* is not in the target dir

--- tests/test_download_file.py
+++ tests/test_download_file.py
@@ -48,3 +48,23 @@ def test_errors(tmpdir, caplog, file_exists):
     else:
         message = 'Expected {0} bytes but got {1} bytes instead.'.format(source_file.size() + 32, source_file.size())
         assert caplog.records()[-2].message == message
+
+
+@pytest.mark.httpretty
+def test_create_directory(capsys, tmpdir):
+    """Sub-directory should be created to store the target file..."""
+    # Prepare requests module mocking.
+    source_file = py.path.local(__file__).dirpath().join('..', 'appveyor_artifacts.py')
+    url = 'https://ci.appveyor.com/api/buildjobs/abc1def2ghi3jkl4/artifacts/appveyor_artifacts.py'
+    httpretty.register_uri(httpretty.GET, url, body=iter(source_file.readlines()), streaming=True)
+
+    # Run.
+    local_path = tmpdir.join('moo', 'appveyor_artifacts.py')
+    download_file(str(local_path), url, source_file.size(), 1024)
+
+    # Check.
+    assert local_path.size() == source_file.size()
+    assert local_path.computehash() == source_file.computehash()
+    stdout, stderr = capsys.readouterr()
+    assert not stdout
+    assert re.match(r'^ => appveyor_artifacts.py [\.]{15,79} [\d]{5,6} bytes\n$', stderr)
